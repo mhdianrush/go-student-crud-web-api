c00af801bed854957ff626ca1a6d5559477e92b6
mhdianrush <mhdianrush@gmail.com>
2023-07-06 15:17:10 +0700

    Develop for Index and Add feature for studentcontroller.go and student views

--- controllers/studentcontroller.go
+++ controllers/studentcontroller.go
@@ -2,15 +2,46 @@ package controllers
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/mhdianrush/go-student-crud-web-api/entities"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Index Running")
+	temp, err := template.ParseFiles("views/student/index.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(w, nil)
 }
 
 func Add(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Add Running")
+	if r.Method == http.MethodGet {
+		temp, err := template.ParseFiles("views/student/add.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(w, nil)
+	} else if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		if err != nil {
+			panic(err)
+		}
+
+		var student entities.Student
+
+		student.FullName = r.FormValue("full_name")
+		student.StudentUniqueId = r.FormValue("student_unique_id")
+		student.Gender = r.FormValue("gender")
+		student.BirthPlace = r.FormValue("birth_place")
+		student.BirthDay = r.FormValue("birth_day")
+		student.Address = r.FormValue("address")
+		student.PhoneNumber = r.FormValue("phone_number")
+
+		fmt.Println(student)
+	}
+
 }
 
 func Edit(w http.ResponseWriter, r *http.Request) {
--- entities/student.go
+++ entities/student.go
@@ -4,7 +4,7 @@ type Student struct {
 	Id              uint   `json:"id"`
 	FullName        string `json:"full_name"`
 	StudentUniqueId string `json:"student_unique_id"`
-	Gender          int8   `json:"gender"`
+	Gender          string `json:"gender"`
 	BirthPlace      string `json:"birth_place"`
 	BirthDay        string `json:"birth_day"`
 	Address         string `json:"address"`
